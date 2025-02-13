9b090c8bb75a4ff00e63c29c61c0312d97f4566f
tennis <kumarteneesh@gmail.com>
2025-02-13 23:23:52 +0530

    added mongoose schema for url details

---
+++ src/models/Url.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const UrlSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  longUrl: { type: String, required: true },
+  shortUrl: { type: String, required: true, unique: true },
+  customAlias: { type: String, unique: true, sparse: true },
+  topic: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model("Url", UrlSchema);
