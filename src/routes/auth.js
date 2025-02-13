931a4dc469605b73753824dea15e52f2c58e6b46
tennis <kumarteneesh@gmail.com>
2025-02-13 23:22:49 +0530

    updated jwt_token to include username as well

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,7 +17,7 @@ router.get("/google/callback", (req, res, next) => {
   
         // Generate JWT Token
         const token = jwt.sign(
-          { id: user._id, googleId: user.googleId, email: user.email },
+          { id: user._id, googleId: user.googleId, email: user.email, name: user.name },
           process.env.JWT_SECRET,
           { expiresIn: "7d" }
         );
